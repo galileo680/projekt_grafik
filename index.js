277725c28c37af21601ef90ad750a12a606786fb
galileo680 <bartek.wicz@gmail.com>
2023-04-08 21:29:07 +0200

    update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,3 +9,4 @@ app.set('view engine', 'ejs');
 app.set('views', views);
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
