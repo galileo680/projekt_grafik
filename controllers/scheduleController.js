847e9645d5af1374cd254f0b9737a5dfc5a2ed37
galileo680 <bartek.wicz@gmail.com>
2023-06-26 17:03:49 +0200

    ostatni update przed nowa baza

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -2,84 +2,22 @@
 const db = require('../util/database');
 const ScheduleModel = require('../models/scheduleModel');
 
-// exports.getSchedule = (req, res) => {
-//   if (req.user.username !== req.params.username) {
-//     return res.status(403).send('Access denied');
-//   }
-
-//   const query = `
-//     SELECT WorkerSchedules.*, Users.username FROM WorkerSchedules
-//     JOIN Users ON WorkerSchedules.userId = Users.id
-//     ORDER BY dayOfWeek, startTime
-// `;
-
-//   db.query(query, [req.user.id], (err, results) => {
-//     if (err) throw err;
-
-//     // Pass the schedule data to the view
-//     //console.log(results);
-//     const errorMessage = req.query.error;
-//     res.render('schedule/schedule', {
-//       username: req.user.username,
-//       schedule: results,
-//       error: errorMessage,
-//     });
-//   });
-// };
-
-// exports.submitSchedule2 = (req, res) => {
-//   // The new schedule to be submitted
-//   const newSchedule = {
-//     dayOfWeek: Number(req.body.dayOfWeek),
-//     startTime: req.body.startTime,
-//     endTime: req.body.endTime,
-//     userId: req.user.id, // Save the userId along with the schedule
-//   };
-
-//   console.log('typeof: ', typeof newSchedule.dayOfWeek);
-//   // Query to get all schedules for the same day of the week
-
-//   const query = 'SELECT * FROM WorkerSchedules WHERE dayOfWeek = ?';
-//   db.query(
-//     query,
-//     [newSchedule.dayOfWeek, newSchedule.userId],
-//     (err, schedules) => {
-//       if (err) throw err;
-
-//       //console.log('schedules: ', schedules);
-
-//       // Check if the new schedule overlaps with any existing schedule
-//       for (let schedule of schedules) {
-//         //console.log('sprawdzam overlapping');
-//         if (
-//           newSchedule.startTime < schedule.endTime &&
-//           newSchedule.endTime > schedule.startTime
-//         ) {
-//           return res.redirect(
-//             `/grafik/${req.user.username}?error=The proposed working hours overlap with an existing schedule.`
-//           );
-//         }
-//       }
-
-//       // If there's no overlap, insert the new schedule
-//       const insertQuery = 'INSERT INTO WorkerSchedules SET ?';
-//       db.query(insertQuery, [newSchedule], (err, results) => {
-//         if (err) throw err;
-//         res.redirect(`/grafik/${req.user.username}`);
-//       });
-//     }
-//   );
-// };
-
 exports.getSchedule = (req, res) => {
   if (req.user.username !== req.params.username) {
     return res.status(403).send('Access denied');
   }
 
-  // Interact with the model to get the data
-  ScheduleModel.getSchedule(req.user.id, (err, results) => {
+  const query = `
+    SELECT WorkerSchedules.*, Users.username FROM WorkerSchedules
+    JOIN Users ON WorkerSchedules.userId = Users.id
+    ORDER BY dayOfWeek, startTime
+`;
+
+  db.query(query, [req.user.id], (err, results) => {
     if (err) throw err;
 
+    // Pass the schedule data to the view
+    //console.log(results);
     const errorMessage = req.query.error;
     res.render('schedule/schedule', {
       username: req.user.username,
@@ -95,23 +33,85 @@ exports.submitSchedule2 = (req, res) => {
     dayOfWeek: Number(req.body.dayOfWeek),
     startTime: req.body.startTime,
     endTime: req.body.endTime,
-    userId: req.user.id,
+    userId: req.user.id, // Save the userId along with the schedule
   };
 
-  // Interact with the model to check the data and submit the schedule
-  ScheduleModel.checkAndSubmitSchedule(
-    newSchedule,
-    req.user.username,
-    (err, overlapExists) => {
+  console.log('typeof: ', typeof newSchedule.dayOfWeek);
+  // Query to get all schedules for the same day of the week
+
+  const query = 'SELECT * FROM WorkerSchedules WHERE dayOfWeek = ?';
+  db.query(
+    query,
+    [newSchedule.dayOfWeek, newSchedule.userId],
+    (err, schedules) => {
       if (err) throw err;
 
-      if (overlapExists) {
-        return res.redirect(
-          `/grafik/${req.user.username}?error=The proposed working hours overlap with an existing schedule.`
-        );
+      //console.log('schedules: ', schedules);
+
+      // Check if the new schedule overlaps with any existing schedule
+      for (let schedule of schedules) {
+        //console.log('sprawdzam overlapping');
+        if (
+          newSchedule.startTime < schedule.endTime &&
+          newSchedule.endTime > schedule.startTime
+        ) {
+          return res.redirect(
+            `/grafik/${req.user.username}?error=The proposed working hours overlap with an existing schedule.`
+          );
+        }
       }
 
-      res.redirect(`/grafik/${req.user.username}`);
+      // If there's no overlap, insert the new schedule
+      const insertQuery = 'INSERT INTO WorkerSchedules SET ?';
+      db.query(insertQuery, [newSchedule], (err, results) => {
+        if (err) throw err;
+        res.redirect(`/grafik/${req.user.username}`);
+      });
     }
   );
 };
+
+// exports.getSchedule = (req, res) => {
+//   if (req.user.username !== req.params.username) {
+//     return res.status(403).send('Access denied');
+//   }
+
+//   // Interact with the model to get the data
+//   ScheduleModel.getSchedule(req.user.id, (err, results) => {
+//     if (err) throw err;
+
+//     const errorMessage = req.query.error;
+//     res.render('schedule/schedule', {
+//       username: req.user.username,
+//       schedule: results,
+//       error: errorMessage,
+//     });
+//   });
+// };
+
+// exports.submitSchedule2 = (req, res) => {
+//   // The new schedule to be submitted
+//   const newSchedule = {
+//     dayOfWeek: Number(req.body.dayOfWeek),
+//     startTime: req.body.startTime,
+//     endTime: req.body.endTime,
+//     userId: req.user.id,
+//   };
+
+//   // Interact with the model to check the data and submit the schedule
+//   ScheduleModel.checkAndSubmitSchedule(
+//     newSchedule,
+//     req.user.username,
+//     (err, overlapExists) => {
+//       if (err) throw err;
+
+//       if (overlapExists) {
+//         return res.redirect(
+//           `/grafik/${req.user.username}?error=The proposed working hours overlap with an existing schedule.`
+//         );
+//       }
+
+//       res.redirect(`/grafik/${req.user.username}`);
+//     }
+//   );
+// };
--- models/scheduleModel.js
+++ models/scheduleModel.js
@@ -12,35 +12,53 @@ const ScheduleModel = {
   },
 
   checkAndSubmitSchedule: function (newSchedule, username, callback) {
-    const query =
-      'SELECT * FROM WorkerSchedules WHERE dayOfWeek = ? AND userId = ?';
+    // const query =
+    //   'SELECT * FROM WorkerSchedules WHERE dayOfWeek = ? AND userId = ?';
+    // db.query(
+    //   query,
+    //   [newSchedule.dayOfWeek, newSchedule.userId],
+    //   (err, schedules) => {
+    //     if (err) return callback(err);
+    //     // Check if the new schedule overlaps with any existing schedule
+    //     for (let schedule of schedules) {
+    //       if (
+    //         newSchedule.startTime < schedule.endTime &&
+    //         newSchedule.endTime > schedule.startTime
+    //       ) {
+    //         // There is an overlap
+    //         return callback(null, true);
+    //       }
+    //     }
+    //     // If there's no overlap, insert the new schedule
+    //     const insertQuery = 'INSERT INTO WorkerSchedules SET ?';
+    //     db.query(insertQuery, [newSchedule], (err) => {
+    //       if (err) return callback(err);
+    //       callback(null, false);
+    //     });
+    //   }
+    // );
+    const query = 'SELECT * FROM WorkerSchedules WHERE dayOfWeek = ?';
 
-    db.query(
-      query,
-      [newSchedule.dayOfWeek, newSchedule.userId],
-      (err, schedules) => {
-        if (err) return callback(err);
+    db.query(query, [newSchedule.dayOfWeek], (err, schedules) => {
+      if (err) return callback(err);
 
-        // Check if the new schedule overlaps with any existing schedule
-        for (let schedule of schedules) {
-          if (
-            newSchedule.startTime < schedule.endTime &&
-            newSchedule.endTime > schedule.startTime
-          ) {
-            // There is an overlap
-            return callback(null, true);
-          }
+      // Create Date objects for the new schedule's start and end times
+      const newStart = new Date(`1970-01-01T${newSchedule.startTime}:00`);
+      const newEnd = new Date(`1970-01-01T${newSchedule.endTime}:00`);
+
+      for (let schedule of schedules) {
+        // Create Date objects for the existing schedule's start and end times
+        const existingStart = new Date(`1970-01-01T${schedule.startTime}:00`);
+        const existingEnd = new Date(`1970-01-01T${schedule.endTime}:00`);
+
+        if (newStart < existingEnd && newEnd > existingStart) {
+          return callback(null, 'overlap');
         }
-
-        // If there's no overlap, insert the new schedule
-        const insertQuery = 'INSERT INTO WorkerSchedules SET ?';
-        db.query(insertQuery, [newSchedule], (err) => {
-          if (err) return callback(err);
-
-          callback(null, false);
-        });
       }
-    );
+
+      const insertQuery = 'INSERT INTO WorkerSchedules SET ?';
+      db.query(insertQuery, [newSchedule], callback);
+    });
   },
 };
 
